799ec21abc002484ef73af934e34dff8720719b6
alessandrobonacchi <alessandro.bonacchi@hotmail.it>
2023-01-03 22:57:47 +0100

    Initial Commit

---
+++ multimedia/multimediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  multimediaApp.swift
+//  multimedia
+//
+//  Created by Alessandro Bonacchi on 03/01/23.
+//
+
+import SwiftUI
+
+@main
+struct multimediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
