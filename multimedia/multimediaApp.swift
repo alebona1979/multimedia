6d71c890055e516eed36b1bdae6fc61990d8c7fa
alessandrobonacchi <alessandro.bonacchi@hotmail.it>
2023-01-18 23:33:59 +0100

    Visual Effects

--- multimedia/multimediaApp.swift
+++ multimedia/multimediaApp.swift
@@ -7,11 +7,18 @@
 
 import SwiftUI
 
+struct VisualEffect: NSViewRepresentable {
+  func makeNSView(context: Self.Context) -> NSView { return NSVisualEffectView() }
+  func updateNSView(_ nsView: NSView, context: Context) { }
+}
+
 @main
 struct multimediaApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView().navigationTitle("Copia file su NAS")
-        }
+            ContentView()
+                .navigationTitle("Copia file su NAS")
+                .background(VisualEffect())
+        }.windowStyle(HiddenTitleBarWindowStyle())
     }
 }
